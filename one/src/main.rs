caa36773840dc7befd2ca4b1889af541fe9cb0af
Daniel Strong <dstrong@glyx.co.uk>
2018-12-03 23:18:16 +0000

    Add (slow/bad?) solution to 1-2

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use itertools::Itertools;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,17 +10,33 @@ fn main() {
     println!("Reading input data from {}", filename);
 
     let mut inputfile = File::open(filename).expect("File not found");
-
     let mut contents = String::new();
     inputfile
         .read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
-    let input: Vec<i32> = contents.trim().split('\n').map(|x| {
-        x.parse::<i32>()
-            .expect("Failed to convert input to numbers (i32)")
-    }).collect();
+    let input = contents
+        .trim()
+        .split('\n')
+        .map(|x| {
+            x.parse::<isize>()
+                .expect("Failed to convert input to numbers (isize)")
+        });
 
-    let result: i32 = input.iter().sum();
+    let result: isize = input.clone().sum();
     println!("The sum is {}", result);
+
+    let partial_sums = input.cycle().coalesce({|prev, cur|
+        Err((prev, prev + cur))
+    });
+
+    let mut seen: Vec<isize> = Vec::new();
+    let first_repeat = partial_sums.clone().find(|&x| {
+        println!("Partial: {:?}", seen);
+        let result = seen.contains(&x);
+        seen.push(x);
+        println!("Seen: {:?}", seen);
+        result
+    }).unwrap();
+    println!("First repetition is {}", first_repeat)
 }
